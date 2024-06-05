9293a25429f004a80eec702a5e0b3797978d4080
Kim Ewing <pekar.kimberly@gmail.com>
2024-06-05 07:19:20 -0600

    adds commented out code that would direct user to login page before main page

--- src/scripts.js
+++ src/scripts.js
@@ -1,6 +1,6 @@
 // This is the JavaScript entry file - your code begins here
 // Do not delete or rename this file ********
-
+// window.location.href = '/login-page.html'
 // An example of how you tell webpack to use a CSS (SCSS) file
 import './css/styles.css';
 
