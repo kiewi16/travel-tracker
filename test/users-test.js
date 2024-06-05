a18b4d5a31d3eddb0c41c6e71a9c571267866574
Kim Ewing <pekar.kimberly@gmail.com>
2024-06-04 21:56:41 -0600

    adds two happy paths for getUserData function. Both tests are passing

--- test/users-test.js
+++ test/users-test.js
@@ -3,6 +3,7 @@ const expect = chai.expect;
 import usersSampleDataset from '../src/data/users-sample-test-data.js';
 import tripsSampleDataset from '../src/data/trips-sample-test-data.js';
 import destinationsSampleDataset from '../src/data/destinations-sample-test-data.js'; 
+const { getUserData } = require('../src/userFunctions.js')
 const allSampleUsers = usersSampleDataset.usersSampleDataset;
 const allSampleTrips = tripsSampleDataset.tripsSampleDataset; 
 const allSampleDestinations = destinationsSampleDataset.destinationsSampleDataset; 
@@ -13,5 +14,29 @@ describe('See if the tests are running', function() {
     console.log("users:", allSampleUsers)
     console.log("trips:", allSampleTrips)
     console.log("destinations:", allSampleDestinations)
+  });
+});
+
+describe('Return User Data', function(){
+  it('should return user data based on their id', function() {
+    const username = "traveler1"
+    const user1 = getUserData(allSampleUsers, username)
+
+    expect(user1).to.deep.equal({
+      "id":1,
+      "name":"Ham Leadbeater",
+      "travelerType":"relaxer"
+    });
+  });
+
+  it.only('should return a different user/s data based on their id', function() {
+    const username = "traveler10"
+    const user10 = getUserData(allSampleUsers, username)
+
+    expect(user10).to.deep.equal({
+      "id":10,
+      "name":"Rickie Jodlowski",
+      "travelerType":"relaxer"
+    });
   });
 });
