1d9aeb5264ac4eff0ed581dcbc525c7a090f5b0e
Kim Ewing <pekar.kimberly@gmail.com>
2024-06-05 13:27:06 -0600

    adds authenticateLogin function and related query selectors and event listener. Function works as intended.

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -1,16 +1,19 @@
-let loginButton = document.getElementById('login-button')
+let loginButton = document.querySelector('#login-button')
+let mainSection = document.querySelector('.main-wrapper')
+let formSection = document.querySelector('.login-form-wrapper')
 
 loginButton.addEventListener('click', authenticateLogin)
 
 function authenticateLogin(event) {
-    // event.preventDefault()
+    event.preventDefault()
 
-    let username = document.getElementById('username').value;
-    let password = document.getElementById('password').value;
+    let username = document.querySelector('#username').value;
+    let password = document.querySelector('#password').value;
 
     if (username === "admin" && password === "password") {
         console.log("test")
-        window.location.href = "../dist/index.html";
+        formSection.classList.add('hidden')
+        mainSection.style.display = 'block'     
     } else {
         alert("Invalid username or password");
     }
