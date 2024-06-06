59bf522f2ae4aa0f19d9c0d6a4ae4772d96fe34f
Kim Ewing <pekar.kimberly@gmail.com>
2024-06-06 17:43:36 -0600

    adds calculateTotalSpentThisYear function and updateMoneySpent function. adds code in authenticateLogin Function that captures user ID from the provided username.

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -1,4 +1,4 @@
-import { getUserData, getDestinationsVisitedByUser } from '../src/userFunctions.js'
+import { getUserData, getDestinationsVisitedByUser, calculateTotalSpentThisYear } from '../src/userFunctions.js'
 import usersSampleDataset from './data/users-sample-test-data.js'
 import tripsSampleDataset from './data/trips-sample-test-data.js'
 import destinationsSampleDataset from './data/destinations-sample-test-data.js'
@@ -10,6 +10,7 @@ const loginButton = document.querySelector('#login-button')
 const formSection = document.querySelector('.login-form-wrapper')
 const mainSection = document.querySelector('.main-wrapper')
 const welcomeMessage = document.querySelector('.welcome-message')
+const totalAmountSpentThisYear = document.querySelector('.total-amount-spent-this-year')
 
 loginButton.addEventListener('click', authenticateLogin)
 
@@ -17,16 +18,17 @@ function authenticateLogin(event) {
     event.preventDefault()
 
     let username = document.querySelector('#username').value;
+    let id = username.split("r").pop();
     let password = document.querySelector('#password').value;
 
-    if (username === "traveler1" && password === "password") {
+    if (username === `traveler${id}` && password === "travel" && id <= 50 && id > 0) {
         formSection.classList.add('hidden')
         mainSection.style.display = 'block'
         preFetchUserData(users, username)
 
     } else {
         alert("Invalid username or password");
-    }  
+    }    
 }
 
 function preFetchUserData(users, username) {
@@ -34,7 +36,9 @@ function preFetchUserData(users, username) {
     const userId = user.id
     updateWelcomeMessage(user)
     const destinationsVisitedByUser = getDestinationsVisitedByUser(trips, destinations, userId)
-    updateTripsTaken(destinationsVisitedByUser )
+    updateTripsTaken(destinationsVisitedByUser)
+    const totalSpentThisYear = calculateTotalSpentThisYear(trips, destinations, userId)
+    updateMoneySpent(totalSpentThisYear)
 }
 
 function updateWelcomeMessage(user) {
@@ -42,11 +46,14 @@ function updateWelcomeMessage(user) {
 }
 
 function updateTripsTaken(destinationsVisitedByUser) {
-    console.log(destinationsVisitedByUser)
     const container = document.querySelector('#destinations-visited-container')
     destinationsVisitedByUser.forEach(destinationVisited => {
         const destinationVisitedText = document.createElement('p')
         destinationVisitedText.innerText =`${destinationVisited}`
         container.appendChild(destinationVisitedText);
     })
+}
+
+function updateMoneySpent(totalSpentThisYear) {
+    totalAmountSpentThisYear.innerText = `Total Amount Spent on Trips this Year: $${totalSpentThisYear}`
 }
