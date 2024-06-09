3af5d995528ec4f33ef68fc14481cb31a8c28208
Kim Ewing <pekar.kimberly@gmail.com>
2024-06-08 19:00:48 -0600

    adds calculateTripCost function

--- src/bookTripFunctions.js
+++ src/bookTripFunctions.js
@@ -9,6 +9,16 @@ function getDestinationId(destinationInput, destinations) {
     return destinationId[0].id
 }
 
+function calculateTripCost(destinationId, destinations, numOfTravelers, duration){
+    const destinationInfo = destinations.filter(destination => {
+        return destination.id === destinationId
+    })
+    // console.log("destinationInfo:", destinationInfo)
+    const tripCost = (destinationInfo[0].estimatedLodgingCostPerDay * duration) + (destinationInfo[0].estimatedFlightCostPerPerson * numOfTravelers)
+    const tripCostWithAgentFee = tripCost * 1.1
+    return tripCostWithAgentFee
+}
 export {
-    getDestinationId
+    getDestinationId,
+    calculateTripCost
 }
