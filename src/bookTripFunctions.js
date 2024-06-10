8dc24120679905b22f1d0c9bf0c47cd298bfcf8b
Kim Ewing <pekar.kimberly@gmail.com>
2024-06-10 17:16:35 -0600

    removes two lines of comments

--- src/bookTripFunctions.js
+++ src/bookTripFunctions.js
@@ -1,6 +1,3 @@
-// pass through the destination.value that is selected by the user
-// generate the destinationId so that it can be passed to the post request function bookTrip()
-
 function getDestinationId(destinationInput, destinations) { 
     const destinationId = destinations.filter(destination => {
         return destination.destination === destinationInput
